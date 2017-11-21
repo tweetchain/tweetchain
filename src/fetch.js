99b95d80b114e0a73d268665a54323e39967a386
Dan Kelly <myself@danielkelly.me>
2017-11-21 14:14:47 -0500

    A little more cleanup

--- src/fetch.js
+++ src/fetch.js
@@ -7,15 +7,10 @@ const twitter = new TwitterService();
 const ots = new OTSService(db);
 const validator = new ValidationService(db, twitter, ots);
 
-try {
-	db.sequelize.sync().then(() => {
-		twitter.connect().then(() => {
-			return validator.sync();
-		}).then(() => {
-			return true;
-		});
+db.sequelize.sync().then(() => {
+	twitter.connect().then(() => {
+		return validator.sync();
+	}).then(() => {
+		return true;
 	});
-} catch(err) {
-	console.log(err);
-	console.trace('Here');
-}
+});
